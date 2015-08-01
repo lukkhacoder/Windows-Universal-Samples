6f15fea7eecc4f24e14d57d02a9e052192b511b5
Lukkha Coder <lukkhacoder@outlook.com>
2015-07-31 17:16:45 -0700

    Added binding to IsOutOfScopeEnabled property
    Added binding to IsOutOfScopeEnabled property

---
+++ CalendarView/CalendarView/Converters/BoolToStringConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace Lukkhacoder.UniversalWindowsSamples.Converters
+{
+    public class BoolToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value == null)
+                return null;
+
+            bool boolean = (bool)value;
+            return boolean.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            if (value == null)
+                return null;
+
+            string booleanString = value.ToString().ToLower();
+            return Boolean.Parse(booleanString);
+
+        }
+    }
+}
